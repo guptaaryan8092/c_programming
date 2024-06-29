a638a73a428985bd9744ef3e43f1a29e554ea8c2
Aryan <aryangupta8092@gmail.com>
2024-06-29 12:27:00 +0530

    add number

---
+++ C_program/2_add_twono_without_usinginput.c
@@ -0,0 +1,9 @@
+//Write a C program for addition of two number without using input function.
+#include<stdio.h>
+int main()
+{
+int a=2,b=3,c;
+c=a+b;
+printf("The sum of two numbers is %d",c);
+return 0;
+}
