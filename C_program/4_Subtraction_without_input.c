eb1936cd6ada2d5f5674fb8cc8899161ba83bb92
Aryan <aryangupta8092@gmail.com>
2024-06-30 11:54:33 +0530

    Subtraction of two number without using input

---
+++ C_program/4_Subtraction_without_input.c
@@ -0,0 +1,9 @@
+//Write a C program for Subtraction of two number without using input function.
+#include <stdio.h>
+int main()
+{
+int a=10,b=4,c;
+c=a-b;
+printf("The difference of two numbers is %d",c);
+return 0;
+}
