6979b175c2fe5d851004736c3a882d7ad12a3925
Aryan <aryangupta8092@gmail.com>
2024-06-30 11:51:29 +0530

    add 2 nos using input

---
+++ C_program/3_add_twono_using_input_fun.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the two numbers \n");
+    scanf("%d\n%d",&a,&b);
+    c=a+b;
+    printf("The sum of the numbers is %d",c);
+    return 0;
+}
