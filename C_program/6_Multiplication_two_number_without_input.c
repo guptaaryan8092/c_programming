9134d9d82fa521aaf44c186806f39566568ec0d9
Aryan <aryangupta8092@gmail.com>
2024-07-05 10:44:14 +0530

    Multiplication of two

---
+++ C_program/6_Multiplication_two_number_without_input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+int a=8,b=4,c;
+c=a*b;
+printf("The multiplication of two numbers is %d",c);
+return 0;
+}
---
+++ C_program/7_Multiplication_two_num_input_function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+float a,b,c;
+printf("Enter the two numbers \n");
+scanf("%f\n%f",&a,&b);
+c=a*b;
+printf("The multiplication of two numbers is %f",c);
+return 0;
+}
